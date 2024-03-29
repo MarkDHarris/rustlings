457bbffff2be2d7ad1ba196259f36b0d58f28980
Mark Harris <mark.d.harris@microsoft.com>
2024-03-29 16:21:23 -0400

    Study session #1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,19 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(a: i32) -> i32 {
+    if a == 35 {
+        70
+    } else if a == 40 {
+        80
+    } else if a == 41 {
+        41
+    } else {
+        a
+    }
+}
 
 // Don't modify this function!
 #[test]
